256cf6ec0f868c3f8dfc694d23763c1260667261
Alberto Campos <albertocamposhdez@hotmail.com>
2020-09-28 22:28:26 -0500

    Fixed test

--- cypress/integration/autenticator.spec.js
+++ cypress/integration/autenticator.spec.js
@@ -7,8 +7,8 @@ describe("Authenticator:", function () {
   describe("Sign In:", () => {
     it("allows a user to signin", () => {
       // Step 2: Take an action (Sign in)
-      cy.get(selectors.usernameInput).type("DUMMY_USERNAME");
-      cy.get(selectors.signInPasswordInput).type("DUMMY_PASSWORD");
+      cy.get(selectors.usernameInput).type("cypress");
+      cy.get(selectors.signInPasswordInput).type("Cypress180");
       cy.get(selectors.signInSignInButton).contains("Sign In").click();
 
       // Step 3: Make an assertion (Check for sign-out text)
